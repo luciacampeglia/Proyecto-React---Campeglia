0253c2ba502dd3ead534d04df212028d6c9edca3
lucia.campeglia <lulacampeglia@gmail.com>
2022-10-20 22:12:23 -0300

    First commit

---
+++ src/Components/CartWidget.js
@@ -0,0 +1,8 @@
+const CartLogo = () =>{
+    return(            
+    <img className="logoCart" src="./Images/shoppingCart" alt="logo-carrito"/> 
+    );
+}
+
+
+export default CartLogo
---
+++ src/Components/Navbar.js
@@ -0,0 +1,23 @@
+import CartLogo from "./CartWidget";
+
+
+const Navbar = () =>{
+    return(            
+    <nav className="navbar navbar-light bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href='link-home' >Frënda clothes</a>
+          
+            <form className="d-flex">
+                <input className="form-control me-2" type="search" placeholder="Filtrar" aria-label="Search"/>
+                <button className="btn btn-outline-light" type="submit">Buscar</button>
+            </form>
+            <CartLogo/>
+        </div>
+    </nav>
+    );
+}
+
+
+
+
+export default Navbar
